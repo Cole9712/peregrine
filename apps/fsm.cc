c46720d2742feb030b6d6863de151a9dd80eaad5
Cole Hao Wu <hwa129@sfu.ca>
2021-11-30 09:09:29 -0800

    modify FSM to compatible for Domain

--- apps/fsm.cc
+++ apps/fsm.cc
@@ -6,6 +6,44 @@
 
 #include "Domain.hh"
 
+template <typename T>
+void organize_vectors(std::vector<Peregrine::SmallGraph> &freq_patterns, std::vector<T> &support)
+{
+  auto inputPatterns = freq_patterns;
+  auto inputSupport = support;
+  std::vector<std::string> str_patterns;
+  freq_patterns.clear();
+  support.clear();
+
+  for (int i = 0; i < inputPatterns.size(); i++)
+  {
+    str_patterns.push_back(inputPatterns[i].to_string());
+  }
+
+  while (inputPatterns.size() > 0)
+  {
+    freq_patterns.push_back(inputPatterns[0]);
+    T tmpSup = inputSupport[0];
+    std::string tmpStr = str_patterns[0];
+    str_patterns.erase(str_patterns.begin());
+    inputSupport.erase(inputSupport.begin());
+    inputPatterns.erase(inputPatterns.begin());
+
+    for (int i = 0; i < str_patterns.size(); i++)
+    {
+      if (tmpStr.compare(str_patterns[i]) == 0)
+      {
+        tmpSup += inputSupport[i];
+        str_patterns.erase(str_patterns.begin() + i);
+        inputSupport.erase(inputSupport.begin() + i);
+        inputPatterns.erase(inputPatterns.begin() + i);
+        i--;
+      }
+    }
+    support.push_back(tmpSup);
+  }
+}
+
 int main(int argc, char *argv[])
 {
   if (argc < 4)
@@ -57,8 +95,9 @@ int main(int argc, char *argv[])
   const auto view = [](auto &&v)
   { return v.get_support(); };
 
-  std::vector<uint64_t> supports;
+  std::vector<DiscoveryDomain<1>> supports;
   std::vector<Peregrine::SmallGraph> freq_patterns;
+  std::vector<uint64_t> supports_result;
 
   std::cout << k << "-FSM with threshold " << threshold << std::endl;
 
@@ -78,53 +117,75 @@ int main(int argc, char *argv[])
     auto psupps = Peregrine::match<Peregrine::Pattern, DiscoveryDomain<1>, Peregrine::AT_THE_END, Peregrine::UNSTOPPABLE>(dg, patterns, nthreads, process, view);
     for (const auto &[p, supp] : psupps)
     {
-      if (supp >= threshold)
-      {
-        freq_patterns.push_back(p);
-        supports.push_back(supp);
-      }
+      freq_patterns.push_back(p);
+      supports.push_back(supp);
+    }
+  }
+
+  organize_vectors<DiscoveryDomain<1>>(freq_patterns, supports);
+  for (int i = 0; i < freq_patterns.size(); i++)
+  {
+    std::cout << freq_patterns[i].to_string() << ": " << supports[i].get_support() << std::endl;
+  }
+  std::vector<Peregrine::SmallGraph> tmp_patterns;
+  for (int i = 0; i < freq_patterns.size(); i++)
+  {
+    if (supports[i].get_support() >= threshold)
+    {
+      tmp_patterns.push_back(freq_patterns[i]);
     }
   }
 
   auto t3 = utils::get_timestamp();
-  std::vector<Peregrine::SmallGraph> patterns = Peregrine::PatternGenerator::extend(freq_patterns, extension_strategy);
+  std::vector<Peregrine::SmallGraph> patterns = Peregrine::PatternGenerator::extend(tmp_patterns, extension_strategy);
 
   const auto process = [](auto &&a, auto &&cm)
   {
     a.map(cm.pattern, cm.mapping);
   };
 
+  std::vector<Domain> supports1;
+  std::cout << "Pattern vector length: " << patterns.size() << std::endl;
   while (step < k && !patterns.empty())
   {
-    std::cout << "Pattern vector length: " << patterns.size() << std::endl;
+
     freq_patterns.clear();
-    supports.clear();
+    supports1.clear();
+    supports_result.clear();
+    tmp_patterns.clear();
     auto psupps = Peregrine::match<Peregrine::Pattern, Domain, Peregrine::AT_THE_END, Peregrine::UNSTOPPABLE>(dg, patterns, nthreads, process, view);
 
     for (const auto &[p, supp] : psupps)
     {
-      if (supp >= threshold)
-      {
-        freq_patterns.push_back(p);
-        supports.push_back(supp);
-      }
+      freq_patterns.push_back(p);
+      supports1.push_back(supp);
+    }
+
+    organize_vectors<Domain>(freq_patterns, supports1);
+    for (int i = 0; i < freq_patterns.size(); i++)
+    {
+      std::cout << freq_patterns[i].to_string() << ": " << supports1[i].get_support() << std::endl;
     }
     for (int i = 0; i < freq_patterns.size(); i++)
     {
-      std::cout << freq_patterns[i].to_string() << ": " << supports[i] << std::endl;
+      if (supports1[i].get_support() >= threshold)
+      {
+        tmp_patterns.push_back(freq_patterns[i]);
+        supports_result.push_back(supports1[i].get_support());
+      }
     }
 
-    patterns = Peregrine::PatternGenerator::extend(freq_patterns, extension_strategy);
-    std::cout << "Nnumber of new patterns: " << patterns.size() << std::endl;
+    patterns = Peregrine::PatternGenerator::extend(tmp_patterns, extension_strategy);
+    std::cout << "Number of new patterns: " << patterns.size() << std::endl;
 
     step += 1;
   }
   auto t2 = utils::get_timestamp();
 
-  std::cout << freq_patterns.size() << " frequent patterns: " << std::endl;
-  for (uint32_t i = 0; i < freq_patterns.size(); ++i)
+  std::cout << tmp_patterns.size() << " frequent patterns: " << std::endl;
+  for (uint32_t i = 0; i < tmp_patterns.size(); ++i)
   {
-    std::cout << freq_patterns[i].to_string() << ": " << supports[i] << std::endl;
+    std::cout << tmp_patterns[i].to_string() << ": " << supports_result[i] << std::endl;
   }
 
   std::cout << "Part 1 finished in " << (t3 - t1) / 1e6 << "s" << std::endl;
